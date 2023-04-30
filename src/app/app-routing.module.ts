83df8bf16ca3b89415aa032c7c49321990ce1659
Tiago Silva <tiagobsb84@gmail.com>
2023-04-30 07:27:45 -0300

    Implementando a secao do card finalizado o estilo e html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  { path: "", redirectTo: "produtos", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
