33cd9bd332609ca4f7ecec5d9043643397bae692
Tiago Silva <tiagobsb84@gmail.com>
2023-04-30 15:18:04 -0300

    Implementada pagina nao encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradoComponent } from './components/nao-encontrado/nao-encontrado.component';
 
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
-  { path: "", redirectTo: "produtos", pathMatch: "full" }
+  { path: "", redirectTo: "produtos", pathMatch: "full" },
+  { path: "**", component: NaoEncontradoComponent }
 ];
 
 @NgModule({
