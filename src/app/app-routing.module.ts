e8c6af8b7e9d48f489baadcc4365aa2be4c829c7
Tiago Silva <tiagobsb84@gmail.com>
2023-05-04 22:59:50 -0300

    Adicionando modulo para pagina de contatos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   { path: "", redirectTo: "produtos", pathMatch: "full" },
   { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
   { path: "**", component: NaoEncontradoComponent }
 ];
 
